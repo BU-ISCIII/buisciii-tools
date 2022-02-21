dbc513bb7ae096e4da433a673147ac8006aa9294
Alema91 <alberto.lema.blanco@gmail.com>
2022-02-21 16:44:55 +0100

    Start testing rich.table

--- bu_isciii/list.py
+++ bu_isciii/list.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
 
-import json
-import prettytable
+from rich.console import Console
+from rich.table import Table
 
-def print_table ():
+# Pruebas
 
+table = Table(title="BU-ISCIII Tools")
 
-"""Lists available bu-isciii pipelines and versions."""
+table.add_column("Released", justify="right", style="cyan", no_wrap=True)
+table.add_column("Title", style="magenta")
+table.add_column("Box Office", justify="right", style="green")
 
-def list_pipelines():
-    """Prints out a list of all bu-isciii pipelines.
-    Args:
-        filter_by (list): A list of strings that can be used for filtering.
-        sort_by (str): workflows can be sorted by keywords. Keyword must be one of
-            `release` (default), `name`, `stars`.
-        as_json (boolean): Set to true, if the lists should be printed in JSON.
+table.add_row("Dec 20, 2019", "Star Wars: The Rise of Skywalker", "$952,110,690")
+table.add_row("May 25, 2018", "Solo: A Star Wars Story", "$393,151,347")
+table.add_row("Dec 15, 2017", "Star Wars Ep. V111: The Last Jedi", "$1,332,539,889")
+table.add_row("Dec 16, 2016", "Rogue One: A Star Wars Story", "$1,332,439,889")
 
-    """
+console = Console()
+console.print(table)
+
+def list_pipelines ():
+    
