999c3b0a9d0f4b5e148b3397a66d534d691b3f99
GuilleGorines <guillermo.gorines@gmail.com>
2022-02-21 17:32:03 +0100

    started delete, rename and revert methods

--- bu_isciii/clean.py
+++ bu_isciii/clean.py
@@ -35,11 +35,13 @@ import os
 # Local imports
 
 class CleanUp(self):
-    def __init__(self,):
+    def __init__(self,resolution_name):
         # access the api/the json/the whatever with the service name to obtain
+        
+        self.resolution_name = resolution_name
         self.base_directory =
-        self.removes =
-        self.renames =
+        self.delete =
+        self.nocopy =
 
 
     def show_removable_dirs(self, to_stdout = True):
@@ -53,10 +55,10 @@ class CleanUp(self):
             to_stdout [BOOL]: if True, print the list. If False, return the list.
         """
         if to_stdout:
-            print(self.removes)
+            print(self.delete)
             return
         else:
-            return self.removes
+            return self.delete
 
     def show_nocopy_dirs(self, to_stdout = True):
         """
@@ -70,12 +72,12 @@ class CleanUp(self):
         """
 
         if to_stdout:
-            print(self.renames)
+            print(self.nocopy)
             return
         else:
-            return self.renames
+            return self.nocopy
 
-    def delete(self, sacredtexts=["lablog","logs"], verbose = True):
+    def delete(self, sacredtexts=['lablog','logs'], verbose = True):
         """
         Remove the files that must be deleted for the delivery of the service
         Their contains, except for the lablog file, and the logs dir, will be
@@ -89,17 +91,27 @@ class CleanUp(self):
 
         """
 
-        for _, dirs, _ in os.walk():
-            if
+        for root, dirs, _ in os.walk():
+            # if there's at least one dir
+            if len(dirs) > 0:
+                for directory in dirs:
+                    if directory in self.delete:
+                        # generate the directory path:
+                        to_be_deleted_dir = os.path.join(root,directory)
+                        
+                        for content in os.listdir(to_be_deleted_dir):
 
-        for item_to_remove in self.removes:
-            for content in os.listdir(item_to_remove):
-                if content not in sacredtexts:
-                    os.remove(content)
-                    if verbose:
-                        print(f'Removed: {content}')
+                            if content not in sacredtexts:
+                                
+                                file_to_be_deleted = os.path.join(to_be_deleted_dir,content)
+                                os.remove(file_to_be_deleted)
 
-
+                                if verbose:
+                                    print(f'Removed: {file_to_be_deleted}.')
+                        
+                        new_name = to_be_deleted_dir + '_DEL'
+                        os.replace(to_be_deleted_dir, new_name)
+        return
 
     def rename(self):
         """
@@ -111,15 +123,60 @@ class CleanUp(self):
         Params:
 
         """
-        for _, dirs, _ in os.walk():
+        for _, dirs, folders in os.walk():
 
-        for old_name in self.renames:
+        for old_name in self.nocopy:
             new_name = old_name + '_NC'
             os.rename(old_name, new_name)
 
+        return
+
+    def revert_renaming(self, verbose = True):
+        """
+        Reverts the naming (adding of the _NC tag)
+        """
+        for root, dirs, _ in os.walk():
+            # if there's at least one dir
+            if len(dirs) > 0:
+                for directory in dirs:
+                    if '_NC' in directory:
+                        nc_path = os.path.join(root,directory)
+
+                        reverted_name = directory - '_NC'
+                        reverted_path = os.path.join(root,reverted_name)
+                        
+                        os.replace(nc_path, reverted_path)
+
+                        if verbose:
+                            print(f"Reverted {directory} to {reverted_name}")
+        return 
+
+    def revert_delete_renaming(self, verbose = True):
+        """
+
+        """
+        for root, dirs, _ in os.walk():
+            if len(dirs) > 0:
+                if '_DEL' in directory:
+                    del_path = os.path.join()
 
 
-    def full_clean_job(self):
+        return 
+
+
+    def full_clean_job(self):     
+        
+        """
+        Perform the whole cleaning of the service 
+        """
+
+        if '_NC' in ____ or '_DEL' in ___:
+
+            # print that a previous usage of this was detected
+
+        
+
         self.rename()
         self.delete()
 
+        return 
