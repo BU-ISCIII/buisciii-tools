3d6c040a132fedde35d69a49b58fb2cfa7ab7cfc
svarona <sarai.var.fer@gmail.com>
2022-02-21 18:39:58 +0100

    Fixed linting format for pep8

--- templates/sars_viralrecon/ANALYSIS/ANALYSIS01_AMPLICONS_HUMAN/percentajeNs.py
+++ templates/sars_viralrecon/ANALYSIS/ANALYSIS01_AMPLICONS_HUMAN/percentajeNs.py
@@ -7,11 +7,11 @@ parser.add_argument('input_dir', type=str, help='Input dir masked files')
 parser.add_argument('output_file', type=str, help='Output file for Ns count')
 args = parser.parse_args()
 
-out_handle = open(args.output_file,"w")
+out_handle = open(args.output_file, "w")
 
 for f in os.listdir(args.input_dir):
     if f.endswith('.masked.fa'):
-        ffpath=os.path.join(args.input_dir,f)
+        ffpath = os.path.join(args.input_dir, f)
         for record in SeqIO.parse(ffpath, "fasta"):
             n_count = record.seq.count("N") + record.seq.count("n")
             out_handle.write("%s\t%0.2f\n" % (record.id, n_count*100.0/len(record)))
