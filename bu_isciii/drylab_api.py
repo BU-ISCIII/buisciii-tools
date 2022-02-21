0a7c77921b97e5a11d24f03a0fc77d67c7dfb255
Sara Monzón <sara.monzon.fdz@gmail.com>
2022-02-21 18:53:05 +0100

    fixed linting for drylab api

--- bu_isciii/drylab_api.py
+++ bu_isciii/drylab_api.py
@@ -2,32 +2,35 @@
 import requests
 import json
 
+
 class RestServiceApi:
     def __init__(self, server, url):
         self.request_url = server + url
-        self.headers = {"content-type": "application/json",}
-
+        self.headers = {
+            "content-type": "application/json",
+        }
 
     def get_request(self, request_info, parameter, value):
-        url_http = str(self.request_url + request_info +'?'+ parameter + '=' + value)
+        url_http = str(self.request_url + request_info + "?" + parameter + "=" + value)
         try:
-            r = requests.get(url_http,  headers=self.headers)
+            r = requests.get(url_http, headers=self.headers)
             return json.loads(r.text)
         except:
             return False
 
-    def put_request(self, request_info,parameter, value):
-        url_http = str(self.request_url + request_info +'?'+ parameter + '=' + value)
+    def put_request(self, request_info, parameter, value):
+        url_http = str(self.request_url + request_info + "?" + parameter + "=" + value)
         try:
-            r = requests.get(url_http,  headers=self.headers)
+            r = requests.get(url_http, headers=self.headers)
             return True
         except:
             return False
 
-''' Example usage
+
+""" Example usage
     rest_api = RestServiceApi('http://localhost:8000/', 'drylab/api/')
     services_queue = rest_api.get_request('services/','state','queued')
-'''
+"""
 """ Example request for iSkyLIMS
     Request:    'drylab/api/services?state=<servicese_state>'
     Method:     get
