7f98686e603d4cebf141cb82661fcafdab767974
Sara Monzón <sara.monzon.fdz@gmail.com>
2022-02-21 14:39:03 +0100

    fixed spaces, added initial new service

---
+++ bu_isciii/new_service.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+'''
+=============================================================
+HEADER
+=============================================================
+INSTITUTION: BU-ISCIII
+AUTHOR: Sara Monzon; Sarai Varona
+MAIL: smonzon@isciii.es; s.varona@isciii.es
+CREATED: 21-02-2022
+DESCRIPTION:
+OPTIONS:
+
+USAGE:
+
+REQUIREMENTS:
+
+TO DO:
+    -INIT: where to find the needed values
+    -PATH: where to be placed
+        -BASE_DIRECTORY: where is it? How do we know where it is?
+
+
+    -NAMING: let's be honest, those are terrible
+================================================================
+END_OF_HEADER
+================================================================
+'''
+# Generic imports
+import sys
+import os
+
+# Local imports
+
+class CleanUp(self):
+    def __init__(self,):
+        self.base_directory =
+        self.removes =
+        self.renames =
