3f81782809b6bf8358821daf5b9adec68c2c5f1e
luissian <chapado.l@gmail.com>
2022-02-21 13:06:55 +0100

    Described the methods used for iSkyLIMs API requests

--- bu_isciii/drylab_api.py
+++ bu_isciii/drylab_api.py
@@ -28,3 +28,58 @@ class RestServiceApi:
     rest_api = RestServiceApi('http://localhost:8000/', 'drylab/api/')
     services_queue = rest_api.get_request('services/','state','queued')
 '''
+""" Example request for iSkyLIMS
+    Request:    'drylab/api/services?state=<servicese_state>'
+    Method:     get
+    Description:    return all services defined on iSkyLIMS which have the state
+            defined in the requested with the following information:
+                'pk', 'serviceRequestNumber','serviceStatus', 'serviceUserId',
+                'serviceCreatedOnDate', 'serviceSeqCenter', 'serviceAvailableService',
+                'serviceFileExt' , 'serviceNotes
+
+    Request:    'drylab/api/resolution?state=<resolution_state>'
+    Method:     get
+    Description:    return all resolutions defined on iSkyLIMS which have the state
+            defined in the requested with the following information:
+                'pk','resolutionNumber', 'resolutionFullNumber','resolutionServiceID',
+                'resolutionDate', 'resolutionEstimatedDate', 'resolutionOnQueuedDate' ,
+                'resolutionOnInProgressDate' , 'resolutionDeliveryDate' ,
+                'resolutionNotes', 'resolutionPipelines'
+
+    Request:    'drylab/api/serviceFullData?service=<service_nuber>'
+    Method:     get
+    Description:    return the Samples, resolutions, and the information requested
+            when creating the service with the following information:
+            Related to service:
+                'pk', 'serviceRequestNumber','serviceStatus', 'serviceUserId',
+                'serviceCreatedOnDate', 'serviceSeqCenter', 'serviceAvailableService',
+                'serviceFileExt' , 'serviceNotes
+            Related to Resolutions:
+                'pk','resolutionNumber', 'resolutionFullNumber','resolutionServiceID',
+                'resolutionDate', 'resolutionEstimatedDate', 'resolutionOnQueuedDate' ,
+                'resolutionOnInProgressDate' , 'resolutionDeliveryDate' ,
+                'resolutionNotes', 'resolutionPipelines'
+            Related to Samples:
+                'runName', 'projectName', 'sampleName' , 'samplePath'
+
+
+    Request:    'drylab/api/samplesInService?service=<service_number>'
+    Method:     get
+    Description:    Return the <run name> , <project name>, <sample name> and
+            <sample path> for all samples requested in the service
+                'runName', 'projectName', 'sampleName' , 'samplePath'
+
+
+    Request:    'drylab/api/update?resolution=<resolution_state>
+    Method:     put
+    Description:
+
+
+    Request:    'drylab/api/delivery
+    Method:     post
+    Description:
+
+
+
+
+"""
