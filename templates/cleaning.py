b091c8066a32c584657b3e6b18c76ec570b250f0
GuilleGorines <guillermo.gorines@gmail.com>
2022-02-21 11:56:36 +0100

    first commit, added initial methods

---
+++ templates/cleaning.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+'''
+=============================================================
+HEADER
+=============================================================
+INSTITUTION: BU-ISCIII
+AUTHOR: Guillermo J. Gorines Cordero
+MAIL: guillermo.gorines@urjc.es
+VERSION: 1.0
+CREATED: 21-2-2022
+REVISED: 21-2-2022
+REVISED BY:
+DESCRIPTION: 
+OPTIONS:
+
+USAGE:
+
+REQUIREMENTS:
+
+TO DO: 
+================================================================
+END_OF_HEADER
+================================================================
+'''
+# Generic imports
+import sys
+import os
+
+
+class Cleanup(self):
+    def __init__(self,):
+        # access the api with the service name to obtain
+        self.removes = 
+        self.renames = 
+
+
+    def show_to_remove(self, to_stdout = True):
+
+        
+
+        print(self.removes)
+        
+    
+    def show_to_rename(self):
+        print(self.renames)
+
+    def remove(self):
+
+    def doitall(self):
+
+    def rename(self):
+
+
