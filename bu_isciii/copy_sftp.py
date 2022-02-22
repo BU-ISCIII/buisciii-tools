d8c14bbbc08dd50cae97660baac05eee2a2184d8
erika.kvalem <erika.kvalem@gmail.com>
2022-02-22 13:24:43 +0100

    small linting fix

--- bu_isciii/copy_sftp.py
+++ bu_isciii/copy_sftp.py
@@ -14,19 +14,20 @@
 # Copy delivery to sftp (e.g: SARS service)
 # Deliver automatization
 # Copy in sftp
-import json
+# import json
 import sys
 import sysrsync
 
 import argparse
 
 
-from drylab_api import RestServiceApi
+# from drylab_api import RestServiceApi
 
 
 def parser_args(args=None):
     Description = "Copy resolution files to sftp"
     Epilog = """Example usage: python copy_sftp.py --source /home/erika.kvalem/Documents/BU_ISCIII/pruebas_source --destination /home/erika.kvalem/Documents/BU_ISCIII/prubas_destination --options -r --exclusions "*_NC" """
+
     parser = argparse.ArgumentParser(description=Description, epilog=Epilog)
     parser.add_argument(
         "-s", "--source", type=str, help="Directory containing files to transfer"
@@ -41,11 +42,17 @@ def parser_args(args=None):
         "--p", "--protocol", type=str, default=None, help="command to execute"
     )
     parser.add_argument(
-        "--o", "--options", type=str, default=None, help="command parameters"
+        "--o",
+        "--options",
+        type=str,
+        required=False,
+        default=None,
+        help="command parameters",
     )
     parser.add_argument(
         "--e",
         "--exclusions",
+        required=False,
         default=None,
         type=str,
         help="Excluded files in the source folder that will not be transfered",
@@ -69,9 +76,7 @@ def main(args=None):
  destination='/data/bi/sftp/Labvirusres'+ service_number,
  options=["-rlpv","--update","-L","--inplace"],
  exclusions=["*_NC","lablog","work","00-reads","*.sh",".nextflow*","*_DEL","*.R","*.py" ])
-"""
-
-"""
+ 
   command1 = {
     "protocol":"rsync",
     "options":["-rlpv","--update","-L","--inplace"],
