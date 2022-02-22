eecdf9694a0ca1cb9b6bf96bd86caedc9c05e1be
Alema91 <alberto.lema.blanco@gmail.com>
2022-02-22 14:16:09 +0100

    Recuperate more or less the removed version

--- bu_isciii/list.py
+++ bu_isciii/list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import json
+import rich.table
+import rich.console
 
 def print_table():
     """Lists available bu-isciii pipelines and versions.
@@ -11,3 +13,15 @@ def list_pipelines():
     """Prints out a list of all bu-isciii pipelines.
     """
     pass
+
+table = rich.table.Table()
+table.add_column("Service name", justify="right", style="cyan"),
+table.add_column("Version", justify="right", style="green")
+
+table.add_row("viralrecon", "v2.1.0")
+
+console = rich.console.Console()
+console.print(table)
+
+
+
