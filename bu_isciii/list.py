5aa6d9e0bb249e59c2b8cc6a44f5a5557e6535ab
Alema91 <alberto.lema.blanco@gmail.com>
2022-02-22 13:36:57 +0100

    Try to solve the remove of previous branch. Start over again

--- bu_isciii/list.py
+++ bu_isciii/list.py
@@ -3,17 +3,12 @@
 import json
 import prettytable
 
-def print_table ():
-
-
-"""Lists available bu-isciii pipelines and versions."""
+def print_table():
+    """Lists available bu-isciii pipelines and versions.
+    """
+    pass
 
 def list_pipelines():
     """Prints out a list of all bu-isciii pipelines.
-    Args:
-        filter_by (list): A list of strings that can be used for filtering.
-        sort_by (str): workflows can be sorted by keywords. Keyword must be one of
-            `release` (default), `name`, `stars`.
-        as_json (boolean): Set to true, if the lists should be printed in JSON.
-
     """
+    pass
