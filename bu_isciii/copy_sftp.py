c27b67dc51f9086fa4098dd1e85ddec938a49a34
erika.kvalem <erika.kvalem@gmail.com>
2022-02-23 13:32:30 +0100

    api working get request

--- bu_isciii/copy_sftp.py
+++ bu_isciii/copy_sftp.py
@@ -14,19 +14,22 @@
 # Deliver automatization
 # Copy in sftp
 
-# from bdb import set_trace
+from bdb import set_trace
 import json
 import sys
 import sysrsync
 
 import argparse
 
-# from drylab_api import RestServiceApi
+# import requests
+
+
+from drylab_api import RestServiceApi
 
 
 def parser_args(args=None):
     Description = "Copy resolution FOLDER to sftp"
-    Epilog = """Example usage: python copy_sftp.py --source /home/erika.kvalem/Documents/BU_ISCIII/pruebas_source/ --destination /home/erika.kvalem/Documents/BU_ISCIII/prubas_destination --options -r --exclusions "*_NC" """
+    Epilog = """Example usage: python copy_sftp.py --source /home/erika.kvalem/Documents/BU_ISCIII/pruebas_source/ --destination /home/erika.kvalem/Documents/BU_ISCIII/prubas_destination --options -r --exclusions "*_NC" --service_number SRVIIER080"""
 
     parser = argparse.ArgumentParser(description=Description, epilog=Epilog)
     parser.add_argument(
@@ -42,8 +45,6 @@ def parser_args(args=None):
         "--sn",
         "--service_number",
         type=str,
-        required=False,
-        default=None,
         help="Service Number",
     )
     parser.add_argument(
@@ -79,6 +80,8 @@ data = json.load(path)
 
 def main(args=None):
     args = parser_args(args)
+
+    """
     sysrsync.run(
         source=args.source,
         destination=args.destination,
@@ -86,22 +89,47 @@ def main(args=None):
         exclusions=data["exclusions"],
         sync_source_contents=False,
     )
+    """
 
     # Change status in iskylims
-    # rest_api = RestServiceApi("drylab/api/", "https://iskylims.isciiides.es/")
+    rest_api = RestServiceApi("http://iskylims.isciiides.es/", "drylab/api/")
+    services_queue = rest_api.get_request("serviceFullData", "service", "SRVIIER080")
+    print(services_queue.items())
+    # keys_list = list(services_queue)
+    # for i in services_queue:
+    #    print(i, services_queue[i])
+    # key = keys_list[0]
+    # print(key)
 
-
-# import pdb
-
-# pdb.set_trace()
+    # services_queue = rest_api.get_request("serviceFullData", "services", "SRVIIER080")
+    # rest_api.get_request("services", "state", "queued")
+    # print(services_queue)
 
 
 # services_queue = rest_api.put_request("resolution/", "state", "Delivery")
 
-# services_queue = rest_api.get_request("serviceFullData", "service", "service_number")
+# services_queue = rest_api.get_request("serviceFullData", "service", "SRVCNM572_20220209_SARSCOV278_icasas_S/")
 
 
 # def put_request(self, request_info, parameter, value):
+
+"""
+Request:    'drylab/api/serviceFullData?services=<service_nuber>'
+    Method:     get
+    Description:    return the Samples, resolutions, and the information requested
+            when creating the service with the following information:
+            Related to service:
+                'pk', 'serviceRequestNumber','serviceStatus', 'serviceUserId',
+                'serviceCreatedOnDate', 'serviceSeqCenter', 'serviceAvailableService',
+                'serviceFileExt' , 'serviceNotes
+            Related to Resolutions:
+                'pk','resolutionNumber', 'resolutionFullNumber','resolutionServiceID',
+                'resolutionDate', 'resolutionEstimatedDate', 'resolutionOnQueuedDate' ,
+                'resolutionOnInProgressDate' , 'resolutionDeliveryDate' ,
+                'resolutionNotes', 'resolutionPipelines'
+            Related to Samples:
+                'runName', 'projectName', 'sampleName' , 'samplePath'
+"""
 
 
 if __name__ == "__main__":
