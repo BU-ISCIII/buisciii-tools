e0f6953af1a242b637eebfd57d4d75ea79937f67
Alema91 <alberto.lema.blanco@gmail.com>
2022-02-23 17:22:56 +0100

    First contact with add a new method

--- bu_isciii/list.py
+++ bu_isciii/list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 """
  =============================================================
  HEADER
@@ -13,28 +14,50 @@
 import json
 import rich.table
 import rich.console
-from bu_isciii.json_reader import JsonReader
 
 
 class ListServices:
-    def __init__(self, input):
-        # Lists available bu-isciii services and versions.
-        with open(
-            str(self.input),
-            "r",
-        ) as f:
-            data = json.load(f)
+    def __init__(self, json_file):
+        fh = open(json_file)
+        self.json_data = json.load(fh)
+        fh.close()
+        self.service_list = list(self.json_data.keys())
 
-    def list_filter():
-        """Filter available bu-isciii services"""
+    def get_service_list(self):
+        return self.service_list
+
+    def get_service_configuration(self, service):
+        if service in self.service_list:
+            return self.json_data[service]
+        return None
+
+    def get_table(self):
+
+        # Creacion de la tabla
+        table = rich.table.Table()
+        table.add_column("Service name", justify="right", style="cyan")
+        table.add_column("Description", justify="left", style="green")
+        datos = self.json_data
+        servicios = self.service_list
+        for i in servicios:
+            table.add_row(str(i), str(datos[i]["description"]))
+
+        console = rich.console.Console()
+        console.print(table)
 
 
+"""
 # get path real
 with open(
     "/home/alberto.lema/Documents/Desarrollo/buisciii-tools/templates/services.json",
     "r",
 ) as f:
     data = json.load(f)
+
+print(data)
+servicios = list(data.keys())
+servicios_files = data[servicios[1]]["clean"]
+print(servicios_files)
 
 # printing elements dict
 
@@ -45,7 +68,7 @@ table.add_column("Description", justify="left", style="green")
 for i in data.keys():
 
     table.add_row(str(i), str(data[i]["description"]))
-    table.add_row(str(i), str(data[i]["description"]))
 
 console = rich.console.Console()
 console.print(table)
+"""
