64f72a0972ba8956914ca77371c790fbfd09fab0
Alema91 <alberto.lema.blanco@gmail.com>
2022-02-23 12:29:35 +0100

    Black changes list.py

--- bu_isciii/list.py
+++ bu_isciii/list.py
@@ -19,26 +19,34 @@ import rich.console
 import os
 import pdb
 
+
 class ListServices:
     def __init__(self):
-    # Lists available bu-isciii services and versions.
-        with open('/home/alberto.lema/Documents/Desarrollo/buisciii-tools/templates/services.json', 'r') as f:
+        # Lists available bu-isciii services and versions.
+        with open(
+            "/home/alberto.lema/Documents/Desarrollo/buisciii-tools/templates/services.json",
+            "r",
+        ) as f:
             data = json.load(f)
         pass
 
+
 def list_filter():
-    """Filter available bu-isciii services
-    """
+    """Filter available bu-isciii services"""
     pass
+
 
 def list_sort():
-    """Sort available bu-isciii services
-    """
+    """Sort available bu-isciii services"""
     pass
 
+
 # get path real
-with open('/home/alberto.lema/Documents/Desarrollo/buisciii-tools/templates/services.json', 'r') as f:
-  data = json.load(f)
+with open(
+    "/home/alberto.lema/Documents/Desarrollo/buisciii-tools/templates/services.json",
+    "r",
+) as f:
+    data = json.load(f)
 
 # printing elements dict
 
@@ -47,7 +55,7 @@ table.add_column("Service name", justify="right", style="cyan")
 table.add_column("Description", justify="left", style="green")
 
 for i in data.keys():
-    table.add_row(str(i), str (data[i]["description"]))
+    table.add_row(str(i), str(data[i]["description"]))
 
 console = rich.console.Console()
 console.print(table)
