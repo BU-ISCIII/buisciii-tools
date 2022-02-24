38ffc6198d2defbbb1edc91785cdc60b57ad777f
GuilleGorines <guillermo.gorines@gmail.com>
2022-02-24 13:04:46 +0100

    added all imports by now, added logger

--- bu_isciii/clean.py
+++ bu_isciii/clean.py
@@ -32,9 +32,22 @@ END_OF_HEADER
 # Generic imports
 # import sys
 import os
+import logging
 import shutil
+import rich
 
 # Local imports
+import bu_isciii
+import bu_isciii.utils
+from bu_isciii.drylab_api import RestServiceApi
+
+log = logging.getLogger(__name__)
+stderr = rich.console.Console(
+    stderr=True,
+    style="dim",
+    highlight=False,
+    force_terminal=bu_isciii.utils.rich_force_colors(),
+)
 
 
 class CleanUp:
