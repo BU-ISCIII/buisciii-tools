56fd9c80b2af0cc17b9dd3a08898b9b85122f803
Sara Monzón <sara.monzon.fdz@gmail.com>
2022-02-24 14:39:44 +0100

    included try/catch, and exiting if failed

--- bu_isciii/new_service.py
+++ bu_isciii/new_service.py
@@ -26,7 +26,7 @@ END_OF_HEADER
 ================================================================
 """
 # Generic imports
-# import sys
+import sys
 import os
 import logging
 
@@ -75,7 +75,7 @@ class NewService:
         for services in self.services_requested:
             service_id_list.append(services["serviceId"])
         service_id_list.append("all")
-        services_sel = bu_isciii.utils.prompt_service_selection(service_id_list)
+        services_sel = [ bu_isciii.utils.prompt_service_selection(service_id_list) ]
         if services_sel == "all":
             services_sel == service_id_list
         return services_sel
@@ -108,18 +108,37 @@ class NewService:
             "[blue]I will copy the template service folders for %s !" % self.full_path
         )
         services_ids = self.get_service_ids()
-        print(services_ids)
-        #services_json = ServiceJson()
-        service_template = ["viralrecon"]  # TMP!!
-        if len(service_template) == 1:
-            shutil.copytree(
-                os.path.join(
-                    os.path.dirname(__file__), "templates", service_template[0]
-                ),
-                self.full_path,
-                dirs_exist_ok=True,
-                ignore=shutil.ignore_patterns("README"),
+        services_json = ServiceJson()
+        if len(services_ids) == 1:
+            try:
+                service_template = services_json.get_find(services_ids[0],"template")
+            except KeyError as e:
+                stderr.print(
+                    "[red]ERROR: Service id %s not found in services json file." % services_ids[0]
+                )
+                #stderr.print("traceback error %s" % e)
+                sys.exit()
+            try:
+                shutil.copytree(
+                    os.path.join(
+                        os.path.dirname(__file__), "templates", service_template[0]
+                    ),
+                    self.full_path,
+                    dirs_exist_ok=True,
+                    ignore=shutil.ignore_patterns("README"),
+                )
+            except OSError as e:
+                stderr.print(
+                    "[red]ERROR: Copying template failed."
+                )
+                #stderr.print("traceback error %s" % e)
+                sys.exit()
+        else:
+            stderr.print(
+                "[red] ERROR: I'm not already prepared for handling more than one error at the same time, sorry! Please re-run and select one of the service ids."
             )
+            sys.exit()
+            return False
         return True
 
     def get_resolution_id(self):
