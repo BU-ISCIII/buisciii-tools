8124c666a93e8b8c34bb907f9db1cbf0cf022a83
Alema91 <alberto.lema.blanco@gmail.com>
2022-02-24 12:42:31 +0100

    Change the init of the class

--- bu_isciii/list.py
+++ bu_isciii/list.py
@@ -14,22 +14,12 @@
 import json
 import rich.table
 import rich.console
+from bu_isciii.json_reader import JsonReader
 
 
 class ListServices:
     def __init__(self, json_file):
-        fh = open(json_file)
-        self.json_data = json.load(fh)
-        fh.close()
-        self.service_list = list(self.json_data.keys())
-
-    def get_service_list(self):
-        return self.service_list
-
-    def get_service_configuration(self, service):
-        if service in self.service_list:
-            return self.json_data[service]
-        return None
+        data_json = JsonReader.get_json_data(json_file)
 
     def get_table(self):
 
@@ -59,9 +49,10 @@ class ListServices:
             return None
 
 
-"""
-prueba = ListServices("../templates/services.json")
-prueba.get_table()
-prueba.get_find()
+prueba = ListServices("templates/services.json")
+prueba()
+
+print(prueba.json_data)
+
+prueba = ListServices("templates/services.json")
 prueba.get_find("assembly_annotation", "clean")
-"""
