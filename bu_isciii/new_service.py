3e2f0a3badf0615a81408bb59b08a18bec3aa726
Sara Monzón <sara.monzon.fdz@gmail.com>
2022-02-24 16:55:09 +0100

    simplify header new service

--- bu_isciii/new_service.py
+++ bu_isciii/new_service.py
@@ -8,19 +8,6 @@ AUTHOR: Sara Monzon; Sarai Varona
 MAIL: smonzon@isciii.es; s.varona@isciii.es
 CREATED: 21-02-2022
 DESCRIPTION:
-OPTIONS:
-
-USAGE:
-
-REQUIREMENTS:
-
-TO DO:
-    -INIT: where to find the needed values
-    -PATH: where to be placed
-        -BASE_DIRECTORY: where is it? How do we know where it is?
-
-
-    -NAMING: let's be honest, those are terrible
 ================================================================
 END_OF_HEADER
 ================================================================
