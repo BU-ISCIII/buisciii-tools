06eb259be200678f4118b695028c70a8baed4b9a
Alema91 <alberto.lema.blanco@gmail.com>
2022-02-24 13:35:34 +0100

    Add two new functionabilities: get dict data from json and items from bu-isciii servicies

--- bu_isciii/service_json.py
+++ bu_isciii/service_json.py
@@ -14,12 +14,39 @@ class ServiceJson:
         self.service_list = list(self.json_data.keys())
 
     def get_json_data(self):
+        """
+        Description:
+            Get the dictionary json data
+        """
         return self.json_data
 
     def get_service_list(self):
+        """
+        Description:
+            Get the servicie list
+        """
         return self.service_list
 
     def get_service_configuration(self, service):
+        """
+        Description:
+            Obtain the servicie configuration from json data
+        """
         if service in self.service_list:
             return self.json_data[service]
         return None
+
+    def get_find(self, service, found):
+        """
+        Owner: Pablo
+        Description:
+            Obtain from servicie any forward items from json data
+        """
+        if found in self.json_data[service]:
+            return self.json_data[service][found]
+        else:
+            for key, value in self.json_data[service].items():
+                if isinstance(value, dict):
+                    if found in self.json_data[service][key]:
+                        return self.json_data[service][key][found]
+            return None
