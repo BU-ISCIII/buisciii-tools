cf4553774ff6f1cbc5c844639b7094716b4f83bb
Alema91 <alberto.lema.blanco@gmail.com>
2022-02-24 14:09:17 +0100

    Now i've removed the json import

--- bu_isciii/list.py
+++ bu_isciii/list.py
@@ -11,7 +11,6 @@
  ================================================================
 """
 
-import json
 import rich.table
 import rich.console
 from bu_isciii.service_json import ServiceJson
@@ -19,27 +18,28 @@ from bu_isciii.service_json import ServiceJson
 
 class ListServices:
     def __init__(self):
+
         service_json = ServiceJson()
-        service_data = service_json.get_json_data()
-        print(service_data)
+        self.service_data = service_json.get_json_data()
+        self.service_list = service_json.get_service_list()
 
     def get_table(self):
 
         # Creacion de la tabla
+
         table = rich.table.Table()
         table.add_column("Service name", justify="right", style="cyan")
         table.add_column("Description", justify="left", style="green")
-        datos = self.json_data
-        servicios = self.service_list
-        for i in servicios:
-            table.add_row(str(i), str(datos[i]["description"]))
+
+        for i in self.service_list:
+            table.add_row(str(i), str(self.service_data[i]["description"]))
 
         console = rich.console.Console()
         console.print(table)
 
 
 prueba = ListServices()
-prueba = ServiceJson()
+prueba.get_table()
 
 
 prueba.get_find("assembly_annotation", "clean")
