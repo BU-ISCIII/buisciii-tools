6ae74144bdf31758cee731873540cf7ce8229787
Alema91 <alberto.lema.blanco@gmail.com>
2022-03-02 17:23:05 +0100

    Now you can filter the list with a name. Need to program if the value is empty

--- bu_isciii/list.py
+++ bu_isciii/list.py
@@ -21,29 +21,23 @@ class ListServices:
     def __init__(
         self,
     ):
-
         service_json = ServiceJson()
         self.service_data = service_json.get_json_data()
         self.service_list = service_json.get_service_list()
 
-    def get_filtered(self, name):
-        """
-        Filter the table
-        """
-        r = re.compile(name)
-        newlist = list(filter(r.match, self.service_list))
-        print(newlist)
-
-    def get_table(self):
+    def get_table(self, name):
         """
         Table print for services names and description
         """
+        busqueda = re.compile(name)
+        nueva_lista = list(filter(busqueda.match, self.service_list))
+
         table = rich.table.Table()
         table.add_column("Service name", justify="right", style="cyan")
         table.add_column("Description", justify="left", style="green")
         table.add_column("Github", justify="left", style="green")
 
-        for i in self.service_list:
+        for i in nueva_lista:
             table.add_row(
                 str(i),
                 str(self.service_data[i]["description"]),
@@ -56,6 +50,4 @@ class ListServices:
 
 prueba = ListServices()
 
-prueba.get_filtered("prueba")
-
-print(prueba.service_list)
+prueba.get_table()
