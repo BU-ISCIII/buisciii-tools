1b25728c7f245721ecb06930d0f22d59fc46f191
GuilleGorines <guillermo.gorines@gmail.com>
2023-01-26 11:02:33 +0100

    I chuckled when I saw that

--- bu_isciii/utils.py
+++ bu_isciii/utils.py
@@ -42,17 +42,15 @@ def prompt_year(lower_limit,
         try:
             # test if it is an int
             if int(year) < lower_limit:
-                stderr.print(f"Sorry, but the oldest record we have is from the year {lower_limit}! \
-                               Year {year} is maybe too... Vintage. Please, try again!")
-            elif int(year) > lower_limit:
-                stderr.print(f"Sorry, but the time machine has not been invented... Yet. \
-                               Year {year} is maybe too... Futuristic. Please, try again!")
+                stderr.print(f"Sorry, but the oldest record we have is from the year {lower_limit}! Year {year} is maybe too... Vintage. Please, try again!")
+            elif int(year) > upper_limit:
+                stderr.print(f"Sorry, but the time machine has not been invented... Yet. Year {year} is maybe too... Futuristic. Please, try again!")
             else:
                 return year
         
         except ValueError:
             stderr.print(f"Ooops, seems like the answer '{year}' is not a year! Please specify the year for which you want to archive services.")
-            year = questionary.text("Year").unsafe_ask()
+            # year = questionary.text("Year").unsafe_ask()
     
     return
 
