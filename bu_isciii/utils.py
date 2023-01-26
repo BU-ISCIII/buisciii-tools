a7a55e80e77ca527a5173a741adeecb2a8553414
GuilleGorines <guillermo.gorines@gmail.com>
2023-01-26 11:04:45 +0100

    simpler like this

--- bu_isciii/utils.py
+++ bu_isciii/utils.py
@@ -41,9 +41,11 @@ def prompt_year(lower_limit,
         year = questionary.text("Year").unsafe_ask()
         try:
             # test if it is an int
-            if int(year) < lower_limit:
+            year = int(year)
+
+            if year < lower_limit:
                 stderr.print(f"Sorry, but the oldest record we have is from the year {lower_limit}! Year {year} is maybe too... Vintage. Please, try again!")
-            elif int(year) > upper_limit:
+            elif year > upper_limit:
                 stderr.print(f"Sorry, but the time machine has not been invented... Yet. Year {year} is maybe too... Futuristic. Please, try again!")
             else:
                 return year
