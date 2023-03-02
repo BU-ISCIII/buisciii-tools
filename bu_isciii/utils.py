2e155fb4c50a7246599fe3c4f38415f6ce93c2db
GuilleGorines <guillermo.gorines@gmail.com>
2023-03-02 15:53:10 +0100

    new util: prompt service id

--- bu_isciii/utils.py
+++ bu_isciii/utils.py
@@ -33,6 +33,12 @@ def prompt_resolution_id():
     resolution_id = questionary.text("Resolution id").unsafe_ask()
     return resolution_id
 
+def prompt_service_id():
+    stderr.print(
+        "Specify the name service ID for the service you want to create. You can obtain this from iSkyLIMS. eg. SRVCNM564.1"
+    )
+    resolution_id = questionary.text("Service ID").unsafe_ask()
+    return resolution_id
 
 def prompt_year(lower_limit, upper_limit):
     """
