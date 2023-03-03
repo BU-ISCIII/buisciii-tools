e77a8c1199dde52b93ef267c93336b802dc8b70a
GuilleGorines <guillermo.gorines@gmail.com>
2023-03-03 13:59:16 +0100

    corrected flaws in the functioning

--- bu_isciii/archive.py
+++ bu_isciii/archive.py
@@ -599,17 +599,17 @@ class Archive:
             # Check if there is a non-compressed
             for place in file_locations:
                 if os.path.exists(place):
-                    stderr.print(f"Uncompressed service {place.split('/')[-1]} has been found in the destiny folder {'/'.join(place.split('/')[:-1])}, so there should be no problem deleting the compressed file {place.split('/')[-1] + '.tar.gz'}. Deleting.")
+                    stderr.print(f"Uncompressed service {place.split('/')[-1]} has been found in the destiny folder {'/'.join(place.split('/')[:-1])}, so there should be no problem deleting the compressed file {place.split('/')[-1] + '.tar.gz'}. Deleting.\n")
                     os.remove(place + ".tar.gz")
                     
+                else:
+                    stderr.print(f"Uncompressed service {place.split('/')[-1]} NOT FOUND in the folder {'/'.join(place.split('/')[:-1])}")
+                    
+                    if (bu_isciii.utils.prompt_selection("What to do?",["Skip deletion", "Delete anyways"]) == "Skip deletion"):
+                        non_deleted_services.append(place.split("/")[-1])
+                        continue
                     else:
-                        stderr.print(f"Uncompressed service {place.split('/')[-1]} NOT FOUND in the folder {'/'.join(place.split('/')[:-1])}")
-                        
-                        if (bu_isciii.utils.prompt_selection("What to do?",["Skip deletion", "Delete anyways"]) == "Skip deletion"):
-                            non_deleted_services.append(place.split("/")[-1])
-                            continue
-                        else:
-                            os.remove(place + ".tar.gz")
+                        os.remove(place + ".tar.gz")
         
         stderr.print(f"Deleted {2*len(self.services_to_move) - len(non_deleted_services)} compressed services.")
         if len(non_deleted_services) > 0:
