87aaa2f34af46283393108a3e7838cc5f9076c19
jaime.ozaez <jaime.ozaez@gmail.com>
2024-05-30 10:04:00 +0200

    irma_config.sh added to DOC folder

---
+++ bu_isciii/templates/IRMA/DOC/irma_config.sh
@@ -0,0 +1,7 @@
+### PROC config ###
+SINGLE_LOCAL_PROC=8	# local maximum processes
+DOUBLE_LOCAL_PROC=4	# local maximum processes (double this number)
+MATCH_PROC=8		# grid maximum processes for the MATCH
+SORT_PROC=8		# currently not used
+ALIGN_PROC=8		# grid maximum processes for the rough align
+ASSEM_PROC=8		# grid maximum processes for assembly
