1aa39ba33366ccd4750a91730ef983d2f742006d
victor5lm <victor_lopez_5@hotmail.com>
2024-09-11 15:48:29 +0200

    Updated version tag in __main__.py

--- bu_isciii/__main__.py
+++ bu_isciii/__main__.py
@@ -57,7 +57,7 @@ def run_bu_isciii():
     )
 
     # stderr.print("[green]                                          `._,._,'\n", highlight=False)
-    __version__ = "2.1.0"
+    __version__ = "2.2.0"
     stderr.print(
         "[grey39]    BU-ISCIII-tools version {}".format(__version__), highlight=False
     )
