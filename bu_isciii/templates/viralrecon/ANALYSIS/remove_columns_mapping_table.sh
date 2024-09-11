8079bb220a3438d9a200163e563a37a38d95e721
victor5lm <victor_lopez_5@hotmail.com>
2024-09-11 15:55:12 +0200

    Updated the remove_columns_mapping_table.sh script

--- bu_isciii/templates/viralrecon/ANALYSIS/remove_columns_mapping_table.sh
+++ bu_isciii/templates/viralrecon/ANALYSIS/remove_columns_mapping_table.sh
@@ -3,7 +3,8 @@
 output_file=$(echo processed_mapping_illumina_$(date '+%Y%m%d').tab)
 
 # Removal of the first three columns of the mapping illumina tab file
-cut --complement -f1-3 *.tab > output_file
+cut --complement -f1-3 mapping_*.tab > output_file
+mv $output_file mapping_illumina_$(date '+%Y%m%d').tab
 
 # Success message
 if [ $? -eq 0 ]; then
