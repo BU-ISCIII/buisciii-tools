150f0ed963ab0e5997ad9ea9484ce4ce13a1e4bd
victor5lm <victor_lopez_5@hotmail.com>
2024-12-23 12:26:02 +0100

    Changed /data/bi/ by /data/ucct/bi/ in all necessary files

--- tests/archived/bi/research/20230101_TESTRESEARCHP_smonzon_C/ANALYSIS/_01_copy_folder.sh
+++ tests/archived/bi/research/20230101_TESTRESEARCHP_smonzon_C/ANALYSIS/_01_copy_folder.sh
@@ -1,1 +1,1 @@
-srun --partition short_idx rsync -rlv /data/bi/scratch_tmp/bi/SRVCNM787_20221205_ASSEMBLY315_lherrera_S/ANALYSIS /scratch/bi
+srun --partition short_idx rsync -rlv /data/ucct/bi/scratch_tmp/bi/SRVCNM787_20221205_ASSEMBLY315_lherrera_S/ANALYSIS /scratch/bi
--- tests/archived/bi/services_and_colaborations/CNM/virology/SRVCNM786_20221205_GENOMEORTHORNAVIRAE02_mdfernandez_S/ANALYSIS/_02_copy_back.sh
+++ tests/archived/bi/services_and_colaborations/CNM/virology/SRVCNM786_20221205_GENOMEORTHORNAVIRAE02_mdfernandez_S/ANALYSIS/_02_copy_back.sh
@@ -1,1 +1,1 @@
-srun --partition short_idx rsync -rlv --exclude "work" /scratch/bi/ANALYSIS/* /data/bi/scratch_tmp/bi/SRVCNM786_20221205_GENOMEORTHORNAVIRAE02_mdfernandez_S/ANALYSIS/
+srun --partition short_idx rsync -rlv --exclude "work" /scratch/bi/ANALYSIS/* /data/ucct/bi/scratch_tmp/bi/SRVCNM786_20221205_GENOMEORTHORNAVIRAE02_mdfernandez_S/ANALYSIS/
