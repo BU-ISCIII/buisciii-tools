e8671f420ddb811fa972f128878c65649ee69544
Alejandro <aberdur7@gmail.com>
2025-02-05 15:14:48 +0100

    Update QC scripts

--- buisciii/templates/viralrecon/ANALYSIS/get_percentage_LDM.py
+++ buisciii/templates/viralrecon/ANALYSIS/get_percentage_LDM.py
@@ -9,42 +9,38 @@ from collections import defaultdict
 # Get the current date in YYYYMMDD format
 current_date = datetime.now().strftime("%Y%m%d")
 
-# Define the input and output directory patterns and the lineage CSV file pattern
+# Define the input/output directory patterns and the lineage CSV file path
 input_directory_pattern = (
     "./*_viralrecon_mapping/variants/ivar/consensus/bcftools/nextclade"
 )
 output_directory_pattern = "./*_viralrecon_mapping"
 lineage_csv_outbreak_pattern = "/data/ucct/bi/references/outbreakinfo/filtrado_20250123_mutaciones_definitorias_linaje.csv"
 
-# Find directories and files matching the given patterns
+# Locate directories and files matching the specified patterns
 input_directories = glob.glob(input_directory_pattern)
 lineage_csv_outbreak_files = glob.glob(lineage_csv_outbreak_pattern)
 input_directory = input_directories[0]
 lineage_csv_outbreak = lineage_csv_outbreak_files[0]
 
-# Command line arguments
 parser = argparse.ArgumentParser(
     description="Script to process defining lineage mutations."
 )
 parser.add_argument(
     "-other_lineages",
     type=int,
+    choices=[0, 1],
     default=0,
-    help="If set to 1, returns other lineages even when LDM == 1 (default is 0)",
+    help="Include other lineages whether or not the mutation is in the sample. (1, 2)",
 )
 args = parser.parse_args()
 other_lineages_flag = args.other_lineages
 
 
 def create_output_directory(base_directory_pattern):
-    """
-    Create an output directory (named 'LDM') inside the first directory that matches the given pattern.
-    Raises a FileNotFoundError if no matching directory is found.
-    """
     base_directories = glob.glob(base_directory_pattern)
     if not base_directories:
         raise FileNotFoundError(
-            f"No directories matching the pattern were found: {base_directory_pattern}"
+            f"No directories found matching the pattern: {base_directory_pattern}"
         )
     base_dir = base_directories[0]
     ldm_dir = os.path.join(base_dir, "LDM")
@@ -53,28 +49,32 @@ def create_output_directory(base_directory_pattern):
 
 
 def normalize_string(s):
-    """
-    Normalize the input string by stripping spaces, converting to lowercase.
-    If the input is NaN, return an empty string.
-    """
     if pd.isna(s):
         return ""
     return str(s).strip().lower()
 
 
+def classify_ldm(row):
+    if row["in_lineage"] == "Yes" and row["in_sample"] == "Yes":
+        return 1
+    elif row["in_lineage"] == "Yes" and row["in_sample"] == "No":
+        return 0
+    elif row["in_lineage"] == "No" and row["in_sample"] == "Yes":
+        return 2
+    return None
+
+
+def get_other_lineages(mutation, sample_lineage, lineage_all_df):
+    other = lineage_all_df[
+        (lineage_all_df["mutation"] == mutation)
+        & (lineage_all_df["lineage"] != sample_lineage)
+    ]
+    return ",".join(sorted(other["lineage"].unique())) if not other.empty else ""
+
+
 def group_deletions(mutation_list):
-    """
-    Group deletions by gene and format them correctly.
-
-    For each mutation in the list, it searches for the pattern 'GENE:residueX-'
-    (e.g., "gene:AA123-"). Then, it groups contiguous deletion positions and
-    formats them as 'GENE:delstart/end'.
-    """
     deletion_groups = defaultdict(list)
-
-    # Process each mutation in the list
     for mutation in mutation_list:
-        # Look for the pattern 'GENE:residueX-' using regex
         match = re.match(r"([a-zA-Z0-9]+):([a-zA-Z]+)(\d+)-", mutation)
         if match:
             gene = match.group(1)
@@ -82,13 +82,10 @@ def group_deletions(mutation_list):
             deletion_groups[gene].append(position)
 
     formatted_deletions = []
-    # Group positions by gene
     for gene, positions in deletion_groups.items():
         positions.sort()
         ranges = []
         start = end = positions[0]
-
-        # Group contiguous positions into ranges
         for pos in positions[1:]:
             if pos == end + 1:
                 end = pos
@@ -97,7 +94,6 @@ def group_deletions(mutation_list):
                 start = end = pos
         ranges.append((start, end))
 
-        # Format the deletions
         for start, end in ranges:
             if start == end:
                 formatted_deletions.append(f"{gene}:del{start}/{start}")
@@ -108,109 +104,52 @@ def group_deletions(mutation_list):
 
 
 def process_sample(input_file, lineage_csv_outbreak, output_tsv, sample_name):
-    """
-    Process a single sample file:
-      - Reads the CSV file and extracts relevant mutation columns.
-      - Normalizes the lineage and mutations.
-      - Processes substitutions, insertions, and deletions (using group_deletions).
-      - Reads the CSV with defining lineage mutations.
-      - Marks which mutations are present in the sample.
-      - Optionally, includes other lineages that share the same mutation.
-      - Saves the resulting DataFrame as a TSV.
-    """
-    # Read the sample CSV file with ';' as delimiter
     df = pd.read_csv(input_file, sep=";")
-    # Keep only relevant columns and rename 'Nextclade_pango' to 'lineage'
     df = df[
         ["Nextclade_pango", "aaSubstitutions", "aaDeletions", "aaInsertions"]
     ].rename(columns={"Nextclade_pango": "lineage"})
-
-    # Normalize the lineage string from the first row
     sample_lineage = normalize_string(df.iloc[0]["lineage"])
-    detected_mutations = []
+    detected_mutations = set()
 
-    # Add substitutions and insertions
     for mutation_type in ["aaSubstitutions", "aaInsertions"]:
         if pd.notna(df.iloc[0][mutation_type]):
-            mutations = df.iloc[0][mutation_type].split(",")
-            detected_mutations.extend(normalize_string(m) for m in mutations)
+            mutations = map(normalize_string, df.iloc[0][mutation_type].split(","))
+            detected_mutations.update(mutations)
 
-    # Process deletions
     if pd.notna(df.iloc[0]["aaDeletions"]):
-        deletions = df.iloc[0]["aaDeletions"].split(",")
-        processed_deletions = group_deletions(deletions)
-        print(processed_deletions)  # Debug: print the processed deletions
-        detected_mutations.extend(normalize_string(d) for d in processed_deletions)
+        deletions = group_deletions(df.iloc[0]["aaDeletions"].split(","))
+        detected_mutations.update(map(normalize_string, deletions))
 
-    # Read the CSV file containing defining lineage mutations
-    lineage_df = pd.read_csv(lineage_csv_outbreak, sep=",")
-    lineage_df = lineage_df.iloc[:, [3, 0, 9]]
-    lineage_df.columns = ["lineage", "mutation", "type"]
-    # Filter rows matching the sample's lineage (after normalization)
-    lineage_df = lineage_df[
-        lineage_df["lineage"].apply(normalize_string) == sample_lineage
-    ]
+    ldm_df = pd.read_csv(lineage_csv_outbreak, sep=",", usecols=[0, 3, 9])
+    ldm_df.columns = ["mutation", "lineage", "type"]
+    ldm_df["lineage"] = ldm_df["lineage"].apply(normalize_string)
+    ldm_df["mutation"] = ldm_df["mutation"].apply(normalize_string)
+    ldm_mutations = set(ldm_df[ldm_df["lineage"] == sample_lineage]["mutation"])
 
-    if lineage_df.empty:
-        print(
-            f"No defining mutations found for lineage {sample_lineage} in {sample_name}"
-        )
-        return None
+    all_mutations = ldm_mutations.union(detected_mutations)
 
-    lineage_df["mutation"] = lineage_df["mutation"].apply(normalize_string)
-    # Mark mutation as present (1) if it is in the detected mutations list, else 0
-    lineage_df["LDM"] = lineage_df["mutation"].apply(
-        lambda m: 1 if m in detected_mutations else 0
+    output_df = pd.DataFrame({"mutations": list(all_mutations)})
+    output_df["in_lineage"] = output_df["mutations"].apply(
+        lambda m: "Yes" if m in ldm_mutations else "No"
     )
-
-    # Read the full lineage CSV again to later find other lineages for each mutation
-    lineage_all_df = pd.read_csv(lineage_csv_outbreak, sep=",")
-    lineage_all_df = lineage_all_df.iloc[:, [3, 0, 9]]
-    lineage_all_df.columns = ["lineage", "mutation", "type"]
-    lineage_all_df["lineage"] = lineage_all_df["lineage"].apply(normalize_string)
-    lineage_all_df["mutation"] = lineage_all_df["mutation"].apply(normalize_string)
-
-    def get_other_lineages(mutation, sample_lineage):
-        """
-        For a given mutation and sample lineage, returns a comma-separated list
-        of other lineages where the mutation is defining.
-        """
-        other = lineage_all_df[
-            (lineage_all_df["mutation"] == mutation)
-            & (lineage_all_df["lineage"] != sample_lineage)
-        ]
-        if other.empty:
-            return ""
-        else:
-            return ",".join(sorted(other["lineage"].unique()))
-
-    # Depending on the flag, either include all other lineages or only for mutations that were not detected
-    if other_lineages_flag == 1:
-        lineage_df["other_lineages"] = lineage_df.apply(
-            lambda row: get_other_lineages(row["mutation"], sample_lineage), axis=1
-        )
-    else:
-        lineage_df["other_lineages"] = lineage_df.apply(
-            lambda row: (
-                ""
-                if row["LDM"] == 1
-                else get_other_lineages(row["mutation"], sample_lineage)
-            ),
-            axis=1,
-        )
-
-    # Save the processed mutations information as a TSV file
-    lineage_df.to_csv(output_tsv, sep="\t", index=False)
-    return lineage_df
+    output_df["in_sample"] = output_df["mutations"].apply(
+        lambda m: "Yes" if m in detected_mutations else "No"
+    )
+    output_df["LDM"] = output_df.apply(classify_ldm, axis=1)
+    output_df["in_other_lineage"] = output_df.apply(
+        lambda row: (
+            get_other_lineages(row["mutations"], sample_lineage, ldm_df)
+            if row["LDM"] == 2 or (row["LDM"] == 1 and other_lineages_flag == 1)
+            else ""
+        ),
+        axis=1,
+    )
+    output_df.insert(0, "lineage", sample_lineage)
+    output_df.to_csv(output_tsv, sep="\t", index=False)
+    return output_df
 
 
 def process_directory(input_directory, lineage_csv_outbreak, output_directory):
-    """
-    Process all CSV files in the given input directory.
-      - For each CSV, process the sample using process_sample().
-      - Compute a summary with the percentage of detected defining mutations.
-      - Merge the summary with a pre-existing TSV file ('s_gene_combined_metrics.tsv') and save the result.
-    """
     csv_files = [f for f in os.listdir(input_directory) if f.endswith(".csv")]
     summary_results = []
 
@@ -224,25 +163,28 @@ def process_directory(input_directory, lineage_csv_outbreak, output_directory):
         lineage_df = process_sample(
             input_file, lineage_csv_outbreak, output_tsv, sample_name
         )
-
         if lineage_df is not None:
-            detected_percentage = (lineage_df["LDM"].sum() / len(lineage_df)) * 100
+            valid_mutations = lineage_df[lineage_df["LDM"].isin([0, 1])]
+            detected_percentage = (
+                (valid_mutations["LDM"].sum() / len(valid_mutations)) * 100
+                if len(valid_mutations) > 0
+                else 0.0
+            )
             summary_results.append(
                 {"sample": sample_name, "%LDMutations": detected_percentage}
             )
 
-    # Read the combined TSV file to merge with summary results
+    summary_df = pd.DataFrame(summary_results)
+    summary_tsv = os.path.join(output_directory, f"{current_date}_mutation_summary.tsv")
+    summary_df.to_csv(summary_tsv, sep="\t", index=False)
+    print(f"Summary file saved: {summary_tsv}")
+
     try:
         tsv_df = pd.read_csv("./s_gene_combined_metrics.tsv", sep="\t")
     except FileNotFoundError:
         raise FileNotFoundError(
             "TSV file 's_gene_combined_metrics.tsv' not found in the current directory."
         )
-
-    summary_df = pd.DataFrame(summary_results)
-    summary_tsv = os.path.join(output_directory, f"{current_date}_mutation_summary.tsv")
-    summary_df.to_csv(summary_tsv, sep="\t", index=False)
-    print(f"Summary file saved: {summary_tsv}")
 
     # Ensure the 'sample' column is of type string in both DataFrames
     tsv_df["sample"] = tsv_df["sample"].astype(str)
@@ -262,6 +204,5 @@ def process_directory(input_directory, lineage_csv_outbreak, output_directory):
     print(f"Updated TSV file saved in: {updated_tsv_path}")
 
 
-# Create the output directory and process the input directory
 output_directory = create_output_directory(output_directory_pattern)
 process_directory(input_directory, lineage_csv_outbreak, output_directory)
--- buisciii/templates/viralrecon/ANALYSIS/sgene_metrics.sh
+++ buisciii/templates/viralrecon/ANALYSIS/sgene_metrics.sh
@@ -6,63 +6,19 @@ input_bam_dir=$(echo ./*_viralrecon_mapping/variants/bowtie2)
 input_vcf_dir=$(echo ./*_viralrecon_mapping/variants/ivar)
 ref_file="../samples_ref.txt"
 output_file="s_gene_combined_metrics.tsv"
-
-# Check if the lablog file exists in the directory
-if [ ! -f "lablog" ]; then
-    echo "Error: The 'lablog' file was not found."
-    exit 1
-fi
-
-# Extract FASTA and GFF file paths from the lablog file
-FASTA=$(grep --only-matching --perl-regexp "(?<=--fasta\s)(\S+)" lablog)
-GFF=$(grep --only-matching --perl-regexp "(?<=--gff\s)(\S+)" lablog)
-
-# Verify that the paths were successfully extracted
-if [ -z "$FASTA" ] || [ -z "$GFF" ]; then
-    echo "Error: FASTA or GFF file paths were not found in the lablog file."
-    exit 1
-fi
-
-SGENE_OUTPUT="sgene.fna"
-
-# Extract S gene coordinates from the GFF file
-echo "Searching for S gene coordinates in $GFF..."
-read CHROM START END <<< $(awk '$3 == "gene" && /Name=S/ {print $1, $4, $5}' "$GFF")
-
-if [[ -z "$START" || -z "$END" ]]; then
-    echo "Error: S gene coordinates were not found in the GFF file."
-    exit 1
-fi
-
-echo "S gene found at: $CHROM:$START-$END"
-
-# Extract the S gene sequence using samtools faidx
-echo "Extracting S gene sequence from $FASTA..."
-samtools faidx "$FASTA" "$CHROM:$START-$END" > "$SGENE_OUTPUT"
-
-# Verify that extraction was successful
-if [ -s "$SGENE_OUTPUT" ]; then
-    echo "Extraction successful. Saved as $SGENE_OUTPUT"
-else
-    echo "Error: S gene extraction failed."
-    exit 1
-fi
-
-# Modify the header for better clarity
-sed -i "s/>.*/>S_gene/" "$SGENE_OUTPUT"
-
-echo "Extraction process completed. File generated: $SGENE_OUTPUT"
+SGENE_OUTPUT="/data/ucct/bi/references/refgenie/alias/coronaviridae/fasta/NC_045512.2/sgene.fna"
 
 # Create output file with headers
-echo -e "sample\tS-Gene_Ambiguous_Percentage\tAmbiguous_Positions\tS-Gene_Coverage_Percentage\tS-Gene_Frameshifts\tTotal_Unambiguous_Bases" > $output_file
+echo -e "sample\tS-Gene_Ambiguous_Percentage\tS-Gene_Coverage_Percentage\tS-Gene_Frameshifts\tTotal_Unambiguous_Bases" > $output_file
 
+# Read reference mappings into an associative array
 declare -A ref_map
 sed -i -e '$a\' "$ref_file"
 while read -r sample ref genome; do
     ref_map["$sample"]=$ref
 done < "$ref_file"
 
-# Process fasta files
+# Process FASTA files
 for fasta_file in $input_fasta_dir/*.consensus.fa; do
   sample_name=$(basename $fasta_file .consensus.fa)
   
@@ -72,21 +28,21 @@ for fasta_file in $input_fasta_dir/*.consensus.fa; do
     continue
   fi
 
-  # Run BLASTn to obtain S gene coordinates in the consensus sequence
+  # Run BLASTn to get S-gene coordinates in the consensus sequence
   blast_output=$(blastn -query "$SGENE_OUTPUT" -subject "$fasta_file" -outfmt "6 sstart send" | sort -k2,2nr | head -1)
   if [ -z "$blast_output" ]; then
     echo "Error: No BLAST hit found for sample $sample_name" >&2
     continue
   fi
 
-  # Extract dynamic S gene coordinates
+  # Extract dynamic S-gene coordinates
   s_gene_start=$(echo "$blast_output" | awk '{print $1}')
   s_gene_end=$(echo "$blast_output" | awk '{print $2}')
   s_gene_length=$((s_gene_end - s_gene_start + 1))
   
   echo "Sample: $sample_name | S-Gene Start: $s_gene_start | S-Gene End: $s_gene_end"
 
-  # Retrieve the full consensus sequence
+  # Get the full consensus sequence
   full_sequence=$(grep -v '^>' "$fasta_file" | tr -d '\n')
   total_unambiguous_count=$(echo "$full_sequence" | grep -o '[ATCG]' | wc -l)
 
@@ -95,25 +51,27 @@ for fasta_file in $input_fasta_dir/*.consensus.fa; do
   ambiguous_positions=()
   for ((i = 0; i < ${#s_gene_sequence}; i++)); do
     base=${s_gene_sequence:i:1}
-    if [[ ! "$base" =~ [ATCG] ]]; then
+    if [[ ! "$base" =~ [ATCGN] ]]; then
       ambiguous_positions+=($((s_gene_start + i)))
     fi
   done
   ambiguous_bases=${#ambiguous_positions[@]}
   ambiguous_percentage=$(echo "scale=2; $ambiguous_bases / $s_gene_length * 100" | bc)
-  ambiguous_positions_str=$(IFS=,; echo "${ambiguous_positions[*]}")
+  ambiguous_percentage=$(printf "%.2f" "$ambiguous_percentage")
 
   # Calculate S-Gene Coverage Percentage
   bam_file="$input_bam_dir/${sample_name}.sorted.bam"
   depth_output=$(samtools depth -r "$ref_genome:$s_gene_start-$s_gene_end" "$bam_file" 2>/dev/null)
   covered_bases=$(echo "$depth_output" | awk '{if ($3 > 0) print $0}' | wc -l)
   coverage_percentage=$(echo "scale=2; $covered_bases / $s_gene_length * 100" | bc)
+  coverage_percentage=$(printf "%.2f" "$coverage_percentage")
 
   # Count S-Gene Frameshifts
   vcf_file="$input_vcf_dir/consensus/${sample_name}.vcf.gz"
   indels=$(bcftools view -r "$sample_name:$s_gene_start-$s_gene_end" -i 'TYPE="indel"' "$vcf_file" 2>/dev/null | wc -l)
 
-  echo -e "$sample_name\t$ambiguous_percentage\t$ambiguous_positions_str\t$coverage_percentage\t$indels\t$total_unambiguous_count" >> $output_file
+  # Save results to output file
+  echo -e "$sample_name\t$ambiguous_percentage\t$coverage_percentage\t$indels\t$total_unambiguous_count" >> $output_file
 done
 
 echo "Process completed. File generated: $output_file"
