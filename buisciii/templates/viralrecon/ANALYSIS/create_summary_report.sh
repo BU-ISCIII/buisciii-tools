fea1f418fc3a00f518c71b75f7d8d73d56e7baaa
Alejandro <aberdur7@gmail.com>
2025-02-20 11:59:30 +0100

    Update create_summary_report.sh to /data/ucct/bi

--- buisciii/templates/viralrecon/ANALYSIS/create_summary_report.sh
+++ buisciii/templates/viralrecon/ANALYSIS/create_summary_report.sh
@@ -2,8 +2,8 @@
 
 # Define fixed data variables
 RUN=$(ls -l ../../RAW/ | cut -d'/' -f4 | sort -u | grep -v 'total' | head -n1 | rev | cut -d " " -f 2- | rev)
-USER=$(pwd | cut -d '/' -f6 | cut -d '_' -f4)
-HOST=$(pwd | cut -d '/' -f8 | cut -d '_' -f4 | tr '[:upper:]' '[:lower:]' | sed 's/.*/\u&/')
+USER=$(pwd | cut -d '/' -f7 | cut -d '_' -f4)
+HOST=$(pwd | cut -d '/' -f9 | cut -d '_' -f4 | tr '[:upper:]' '[:lower:]' | sed 's/.*/\u&/')
 
 # Define header for output file
 HEADER="run\tuser\thost\tVirussequence\tsample\ttotalreads\treadshostR1\treadshost\t%readshost\treadsvirus\t%readsvirus\tunmappedreads\t%unmapedreads\tmedianDPcoveragevirus\tCoverage>10x(%)\tVariantsinconsensusx10\tMissenseVariants\t%Ns10x\tLineage\tread_length\tanalysis_date"
