edc4bd3f5f7cf295bbe0dbb2787bd20cd52c6642
victor5lm <victor_lopez_5@hotmail.com>
2025-05-13 12:28:16 +0200

    Updated version in pyproject.toml and main.py

--- buisciii/__main__.py
+++ buisciii/__main__.py
@@ -57,7 +57,7 @@ def run_buisciii():
     )
 
     # stderr.print("[green]                                          `._,._,'\n", highlight=False)
-    __version__ = "2.2.8"
+    __version__ = "2.2.9"
     stderr.print(
         "[grey39]    BU-ISCIII-tools version {}".format(__version__), highlight=False
     )
