a1b900bea6fc63e175b278ddf0b4ced3838c9495
victor5lm <victor_lopez_5@hotmail.com>
2025-05-21 11:05:32 +0200

    Updated __main__.py

--- buisciii/__main__.py
+++ buisciii/__main__.py
@@ -57,7 +57,7 @@ def run_buisciii():
     )
 
     # stderr.print("[green]                                          `._,._,'\n", highlight=False)
-    __version__ = "2.2.9"
+    __version__ = "2.2.10"
     stderr.print(
         "[grey39]    BU-ISCIII-tools version {}".format(__version__), highlight=False
     )
