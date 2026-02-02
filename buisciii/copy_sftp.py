88280833101bbd2df3a8b80579e341eb8c0191d8
victor5lm <victor_lopez_5@hotmail.com>
2026-02-02 17:05:29 +0100

    Implemented logging and improved docstrings in copy_sftp.py

--- buisciii/copy_sftp.py
+++ buisciii/copy_sftp.py
@@ -16,6 +16,7 @@ import buisciii.drylab_api
 import buisciii.service_json
 
 log = logging.getLogger(__name__)
+
 stderr = Console(
     stderr=True,
     style="dim",
@@ -44,7 +45,7 @@ class CopySftp:
         self.conf = conf.get_configuration("sftp_copy")
         conf_api = conf.get_configuration("xtutatis_api_settings")
 
-        # Obtain info from iskylims api
+        # Obtain info from iSkyLIMS API
         rest_api = buisciii.drylab_api.RestServiceApi(
             conf_api["server"], conf_api["api_url"], api_user, api_password
         )
@@ -76,14 +77,20 @@ class CopySftp:
             self.path = buisciii.utils.prompt_path(msg="Path")
         elif path == "-a":
             stderr.print(
-                "[red] ERROR: Either give a path or make the terminal ask you a path, not both."
+                "[red]ERROR: Either give a path or make the terminal ask you for a path, not both."
+            )
+            log.error(
+                "ERROR: Either give a path or make the terminal ask you for a path, not both."
             )
             sys.exit()
         elif path is not None and ask_path is False:
             self.path = path
         elif path is not None and ask_path is not False:
             stderr.print(
-                "[red] ERROR: Either give a path or make the terminal ask you a path, not both."
+                "[red]ERROR: Either give a path or make the terminal ask you a path, not both."
+            )
+            log.error(
+                "ERROR: Either give a path or make the terminal ask you a path, not both."
             )
             sys.exit()
         else:
@@ -99,14 +106,14 @@ class CopySftp:
     def get_last_folders(self, services_ids, type="last_folder"):
         """
         Description:
-            Get last folders from service conf
+            Get last folders from service conf.
 
         Usage:
             object.get_last_folders(services_ids, type = "last_folder")
 
         Params:
-            services_ids [list]: list with services ids selected.
-            type [string]: "last_folder" for getting the param from service.json
+            services_ids [list]: list with services IDs selected.
+            type [string]: "last_folder" for getting the param from service.json.
         """
         service_conf = buisciii.service_json.ServiceJson()
         last_folders_list = []
@@ -117,26 +124,42 @@ class CopySftp:
                     last_folders_list.append(item)
             except KeyError as e:
                 stderr.print(
-                    "[red]ERROR: Service id %s not found in services json file."
+                    "[red]ERROR: Service ID %s not found in the services.json file."
                     % service
                 )
-                stderr.print("traceback error %s" % e)
-                sys.exit()
+                log.error(
+                    f"ERROR: Service ID {service} not found in the services.json file."
+                )
+                raise
 
         return last_folders_list
 
     def listdirs(self, final_path):
+        """
+        Description:
+            Print all files contained within a given directory and its subdirectories.
+        """
         for path, subdirs, files in os.walk(final_path):
             for name in files:
                 print(os.path.join(path, name))
 
     def copy_sftp(self):
+        """
+        Description:
+            Copy the service folder to the configured SFTP destination using rsync.
+        """
         if self.service_folder in self.full_path:
             today_date = datetime.today().strftime("%Y%m%d")
             log_command = (
                 "--log-file=" + self.full_path + "/DOC/rsync_" + today_date + ".log"
             )
             self.sftp_options.append(log_command)
+            stderr.print(
+                f"[yellow]The service folder will now be copied to the '{self.sftp_folder}' SFTP folder."
+            )
+            log.info(
+                f"The service folder will be copied to the '{self.sftp_folder}' SFTP folder."
+            )
             try:
                 if self.conf["protocol"] == "rsync":
                     sysrsync.run(
@@ -147,28 +170,31 @@ class CopySftp:
                         sync_source_contents=False,
                     )
                     stderr.print(
-                        "[green] Data copied to the sftp folder successfully",
+                        "[green]Data copied to the SFTP folder successfully!",
                         highlight=False,
                     )
+                    log.info("Data copied to the SFTP folder successfully!")
                 else:
                     stderr.print(
-                        "[ref] This protocol is not allowed at the moment",
+                        "[red]This protocol is not allowed at the moment",
                         highlight=False,
                     )
+                    log.error("This protocol is not allowed at the moment!")
                     sys.exit()
             except RsyncError as e:
-                stderr.print(e)
                 stderr.print(
-                    "[yellow] Data copied to the sftp with errors.",
+                    "[yellow]Data copied to the SFTP with errors.",
                     highlight=False,
                 )
+                log.warning("Data copied to the SFTP with errors.")
+                raise
             finally:
                 for folders_list in self.last_folders:
                     final_folder = os.path.join(
                         self.sftp_folder, self.service_folder, folders_list
                     )
                     stderr.print(
-                        "Listing the content of the final folder %s" % folders_list
+                        "Listing the content of the final folder %s:" % folders_list
                     )
                     self.listdirs(final_folder)
         else:
@@ -176,5 +202,8 @@ class CopySftp:
                 "[red]ERROR: Service number %s not in the source path %s"
                 % (self.service_folder, self.full_path)
             )
-            sys.exit()
+            log.error(
+                f"ERROR: Service number {self.service_folder} not in the source path {self.full_path}"
+            )
+            sys.exit(1)
         return True
