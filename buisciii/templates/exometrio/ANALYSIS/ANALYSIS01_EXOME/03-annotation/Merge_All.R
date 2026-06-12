5641ebe9649d3481876abeee65aae0e478c3f955
paupascualmas <paupascualmas@gmail.com>
2026-06-12 13:50:51 +0200

    Optimizated annotation wgs & exome

--- buisciii/templates/exometrio/ANALYSIS/ANALYSIS01_EXOME/03-annotation/Merge_All.R
+++ buisciii/templates/exometrio/ANALYSIS/ANALYSIS01_EXOME/03-annotation/Merge_All.R
@@ -1,40 +1,109 @@
+library(data.table)
 
 # Cargar la tabla de datos
-plugin <- read.table("dbNSFP_ENSG_plugin_hg19.txt", header = TRUE, sep = "\t", quote = "")
+plugin <- fread(
+  "dbNSFP_ENSG_plugin_hg19.txt",
+  sep = "\t",
+  quote = "",
+  header = TRUE,
+  check.names = TRUE
+)
 
 # Fusionar las cuatro columnas en una nueva columna llamada "ID"
-plugin$ID <- paste(plugin$`chr`, plugin$`hg19_pos`, plugin$ref, plugin$alt, sep = "_")
+plugin[, ID := paste(chr, hg19_pos, ref, alt, sep = "_")]
 
-#Borrar las columnas innecesarias
-plugin <-plugin[,-c(1,2,3,4)]
+# Borrar las columnas innecesarias
+plugin[, c("chr", "hg19_pos", "ref", "alt") := NULL]
 
 # Guardar la tabla resultante
-write.table(plugin, "dbNSFP_ENSG_plugin_Columns.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+fwrite(
+  plugin,
+  "dbNSFP_ENSG_plugin_Columns.txt",
+  sep = "\t",
+  quote = FALSE,
+  na = "-"
+)
 
-#cargamos tabla vep_annot_head
-vep <- read.table("./vep/vep_annot_head.txt",header=T, sep = "\t", quote = "")
+# Cargamos tabla vep_annot_head
+vep <- fread(
+  "./vep/vep_annot_head.txt",
+  sep = "\t",
+  quote = "",
+  header = TRUE,
+  check.names = TRUE
+)
 
-#pegamos las tablas usando de referencia la columnas Uploadaded variation
-vep_plugin <- merge(vep,plugin,by="ID",all.x=T,all.y=F)
+# Pegamos las tablas usando de referencia la columna ID
+vep_plugin <- merge(
+  vep,
+  plugin,
+  by = "ID",
+  all.x = TRUE,
+  all.y = FALSE,
+  sort = TRUE
+)
 
-# guardamos la tabla resultante
-write.table(vep_plugin,file="./vep/vep_plugin.txt",sep="\t", na="-", row.names=F,quote=FALSE)
+# Guardamos la tabla resultante
+fwrite(
+  vep_plugin,
+  file = "./vep/vep_plugin.txt",
+  sep = "\t",
+  na = "-",
+  quote = FALSE
+)
 
-# cargamos tabla dbNSFP y vep_plugin
-dbNSFP <- read.table("dbNSFP_ENSG_gene_GRCh37.txt",header=T, sep = "\t", quote = "")
+# Cargamos tabla dbNSFP y vep_plugin
+dbNSFP <- fread(
+  "dbNSFP_ENSG_gene_GRCh37.txt",
+  sep = "\t",
+  quote = "",
+  header = TRUE,
+  check.names = TRUE
+)
 
-#pegamos dbNSFP con vep usando de referencia la columna gene
-vep_dbNSFP <- merge(vep_plugin,dbNSFP,by="Gene",all.x=T,all.y=F)
+# Pegamos dbNSFP con vep usando de referencia la columna Gene
+vep_dbNSFP <- merge(
+  vep_plugin,
+  dbNSFP,
+  by = "Gene",
+  all.x = TRUE,
+  all.y = FALSE,
+  sort = TRUE
+)
 
-# guardamos la tabla resultante
-write.table(vep_dbNSFP,file="./vep/vep_dbNSFP.txt",sep="\t", na="-", row.names=F,quote=FALSE)
+# Guardamos la tabla resultante
+fwrite(
+  vep_dbNSFP,
+  file = "./vep/vep_dbNSFP.txt",
+  sep = "\t",
+  na = "-",
+  quote = FALSE
+)
 
-# cargamos la tabla vep/variants.table
-variants <- read.table("./vep/variants.table",header=T, sep = "\t", quote = "")
+# Cargamos la tabla vep/variants.table
+variants <- fread(
+  "./vep/variants.table",
+  sep = "\t",
+  quote = "",
+  header = TRUE,
+  check.names = TRUE
+)
 
-# pegamos vep_dbNSFP junto con variants
-vep_dbNSFP_variants <- merge(variants,vep_dbNSFP,by="ID",all.x=F,all.y=F)
+# Pegamos vep_dbNSFP junto con variants
+vep_dbNSFP_variants <- merge(
+  variants,
+  vep_dbNSFP,
+  by = "ID",
+  all.x = FALSE,
+  all.y = FALSE,
+  sort = TRUE
+)
 
-# guardamos la tabla resultante
-write.table(vep_dbNSFP_variants, file="variants_annot_all.tab", sep="\t", na="-", row.names=F, quote=FALSE)
-
+# Guardamos la tabla resultante
+fwrite(
+  vep_dbNSFP_variants,
+  file = "variants_annot_all.tab",
+  sep = "\t",
+  na = "-",
+  quote = FALSE
+)
