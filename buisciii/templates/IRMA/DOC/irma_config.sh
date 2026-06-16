598791a4969b41ab36f10c07a3de4b38430013d4
victor5lm <victor_lopez_5@hotmail.com>
2026-06-16 10:29:39 +0200

    Updated IRMA's software versions and config

--- buisciii/templates/IRMA/DOC/irma_config.sh
+++ buisciii/templates/IRMA/DOC/irma_config.sh
@@ -1,6 +1,6 @@
 ### PROC config ###
-SINGLE_LOCAL_PROC=8	# local maximum processes
-DOUBLE_LOCAL_PROC=4	# local maximum processes (double this number)
+SINGLE_LOCAL_PROC=4	# local maximum processes
+DOUBLE_LOCAL_PROC=2	# local maximum processes (double this number)
 MATCH_PROC=8		# grid maximum processes for the MATCH
 SORT_PROC=8		# currently not used
 ALIGN_PROC=8		# grid maximum processes for the rough align
