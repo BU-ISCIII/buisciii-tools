6153a4dc5aae86256550b441836c81083328ac11
paupascualmas <paupascualmas@gmail.com>
2026-07-10 13:46:27 +0200

    Fix IRMA stats for flu and rsv  when having missing readcounts

--- buisciii/templates/IRMA/ANALYSIS/ANALYSIS01_IRMA/04-irma/create_irma_stats_flu.sh
+++ buisciii/templates/IRMA/ANALYSIS/ANALYSIS01_IRMA/04-irma/create_irma_stats_flu.sh
@@ -1,43 +1,105 @@
+#!/usr/bin/env bash
 
-echo -e "sample_ID\tTotalReads\tMappedReads\t%MappedReads\tFlu_type\tReads_HA\tReads_MP\tReads_NA\tReads_NP\tReads_NS\tReads_PA\tReads_PB1\tReads_PB2" > irma_stats_flu.txt
+OUTPUT="irma_stats_flu.txt"
 
-cat ../samples_id.txt | while read in 
-do
-    SAMPLE_ID=$(echo ${in})
-    TOTAL_READS=""; MAPPEDREADS=""; PCTMAPPED=""; FLU_TYPE=""; HA=""; MP=""; NA=""; NP=""; NS=""; PA=""; PB1=""; PB2=""; HE=""
+echo -e "sample_ID\tTotalReads\tMappedReads\t%MappedReads\tFlu_type\tReads_HA\tReads_MP\tReads_NA\tReads_NP\tReads_NS\tReads_PA\tReads_PB1\tReads_PB2" > "$OUTPUT"
 
-    if test -f ${in}/tables/READ_COUNTS.txt; then
-        TOTAL_READS=$(grep '1-initial' ${in}/tables/READ_COUNTS.txt | cut -f2)
-        MAPPEDREADS=$(grep '3-match' ${in}/tables/READ_COUNTS.txt | cut -f2)
-        PCTMAPPED=$(awk "BEGIN {printf \"%.2f\", ($MAPPEDREADS/$TOTAL_READS)*100}")
-        FLU_TYPE=$(paste <(grep '4-[A-C]' ${in}/tables/READ_COUNTS.txt | cut -f1 | cut -d '_' -f1 | cut -d '-' -f2 | head -n1 ) <(grep '4-[A-B]_HA' ${in}/tables/READ_COUNTS.txt | cut -f1 | cut -d '_' -f3 | cut -d '-' -f2) <(grep '4-[A-B]_NA' ${in}/tables/READ_COUNTS.txt | cut -f1 | cut -d '_' -f3) | tr '\t' '_' | sed 's/_*$//')
-        HA=$(grep '4-[A-C]_HA' ${in}/tables/READ_COUNTS.txt | cut -f2)
-        MP=$(grep '4-[A-C]_MP' ${in}/tables/READ_COUNTS.txt | cut -f2)
-        NA=$(grep '4-[A-C]_NA' ${in}/tables/READ_COUNTS.txt | cut -f2)
-        NP=$(grep '4-[A-C]_NP' ${in}/tables/READ_COUNTS.txt | cut -f2)
-        NS=$(grep '4-[A-C]_NS' ${in}/tables/READ_COUNTS.txt | cut -f2)
-        PA=$(grep '4-[A-C]_PA' ${in}/tables/READ_COUNTS.txt | cut -f2)
-        PB1=$(grep '4-[A-C]_PB1' ${in}/tables/READ_COUNTS.txt | cut -f2)
-        PB2=$(grep '4-[A-C]_PB2' ${in}/tables/READ_COUNTS.txt | cut -f2)
-        #In case of Influenza C in samples:
-        HE=$(grep '4-C_HE' ${in}/tables/READ_COUNTS.txt | cut -f2)
+while read -r in; do
+    [[ -z "$in" ]] && continue
+
+    SAMPLE_ID="$in"
+    FILE="${in}/tables/READ_COUNTS.txt"
+
+    if [[ -f "$FILE" ]]; then
+        read -r TOTAL_READS MAPPEDREADS PCTMAPPED < <(
+            awk -F'\t' '
+                $1=="1-initial" {t=$2}
+                $1=="3-match"   {m=$2}
+                END {
+                    t += 0
+                    m += 0
+
+                    if (t > 0) {
+                        pct = (m / t) * 100
+                    } else {
+                        pct = 0
+                    }
+
+                    printf "%d %d %.2f\n", t, m, pct
+                }
+            ' "$FILE"
+        )
+
+        FLU_TYPE=$(paste \
+            <(grep '4-[A-C]' "$FILE" | cut -f1 | cut -d_ -f1 | cut -d- -f2 | head -n1) \
+            <(grep '4-[A-B]_HA' "$FILE" | cut -f1 | cut -d_ -f3 | cut -d- -f2) \
+            <(grep '4-[A-B]_NA' "$FILE" | cut -f1 | cut -d_ -f3) \
+            | tr '\t' '_' \
+            | sed 's/_*$//'
+        )
+
+        HA=$(grep -m1 '4-[A-C]_HA' "$FILE" | cut -f2)
+        MP=$(grep -m1 '4-[A-C]_MP' "$FILE" | cut -f2)
+        NA=$(grep -m1 '4-[A-C]_NA' "$FILE" | cut -f2)
+        NP=$(grep -m1 '4-[A-C]_NP' "$FILE" | cut -f2)
+        NS=$(grep -m1 '4-[A-C]_NS' "$FILE" | cut -f2)
+        PA=$(grep -m1 '4-[A-C]_PA' "$FILE" | cut -f2)
+        PB1=$(grep -m1 '4-[A-C]_PB1' "$FILE" | cut -f2)
+        PB2=$(grep -m1 '4-[A-C]_PB2' "$FILE" | cut -f2)
+        HE=$(grep -m1 '4-C_HE' "$FILE" | cut -f2)
+
         if [[ -n "$HE" ]]; then
-            LINE=$(paste <(echo $SAMPLE_ID) <(echo $TOTAL_READS) <(echo $MAPPEDREADS) <(echo $PCTMAPPED) <(echo $FLU_TYPE) <(echo $HA) <(echo $MP) <(echo $NA) <(echo $NP) <(echo $NS) <(echo $PA) <(echo $PB1) <(echo $PB2) <(echo $HE))
+            printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" \
+                "$SAMPLE_ID" \
+                "$TOTAL_READS" \
+                "$MAPPEDREADS" \
+                "$PCTMAPPED" \
+                "$FLU_TYPE" \
+                "$HA" \
+                "$MP" \
+                "$NA" \
+                "$NP" \
+                "$NS" \
+                "$PA" \
+                "$PB1" \
+                "$PB2" \
+                "$HE" \
+                >> "$OUTPUT"
         else
-            LINE=$(paste <(echo $SAMPLE_ID) <(echo $TOTAL_READS) <(echo $MAPPEDREADS) <(echo $PCTMAPPED) <(echo $FLU_TYPE) <(echo $HA) <(echo $MP) <(echo $NA) <(echo $NP) <(echo $NS) <(echo $PA) <(echo $PB1) <(echo $PB2))
+            printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" \
+                "$SAMPLE_ID" \
+                "$TOTAL_READS" \
+                "$MAPPEDREADS" \
+                "$PCTMAPPED" \
+                "$FLU_TYPE" \
+                "$HA" \
+                "$MP" \
+                "$NA" \
+                "$NP" \
+                "$NS" \
+                "$PA" \
+                "$PB1" \
+                "$PB2" \
+                >> "$OUTPUT"
         fi
+
     else
         echo "Sample $SAMPLE_ID doesn't have READ_COUNTS.txt file. Skipping"
-        TOTAL_READS=NA
-        MAPPEDREADS=0
-        PCTMAPPED=0
-        LINE=$(paste <(echo $SAMPLE_ID) <(echo $TOTAL_READS) <(echo $MAPPEDREADS) <(echo $PCTMAPPED))
+
+        printf "%s\tNA\t0\t0.00\t\t\t\t\t\t\t\t\t\n" \
+            "$SAMPLE_ID" \
+            >> "$OUTPUT"
     fi
 
-    echo "$LINE" >> irma_stats_flu.txt
-done
+done < ../samples_id.txt
 
-ANY_C=$(grep "C" irma_stats_flu.txt)
-if [[ -n "$ANY_C" ]]; then
-    sed -i 's/Reads_PB2/Reads_PB2\tReads_HE/g' irma_stats_flu.txt
+if awk -F'\t' '
+    NR > 1 && NF >= 14 && $14 != "" {
+        found = 1
+    }
+    END {
+        exit(found ? 0 : 1)
+    }
+' "$OUTPUT"; then
+    sed -i '1s/Reads_PB2$/Reads_PB2\tReads_HE/' "$OUTPUT"
 fi
